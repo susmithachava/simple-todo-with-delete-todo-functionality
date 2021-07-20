ad435f58342c368c3c9e6c89e6973dc059b73bba
Chava Susmitha <susmi.chava@gmail.com>
2021-07-20 08:05:27 +0000

    simple todo with delete todo functionality

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {todoDetails, deleteTodo} = props
+  const {id, title} = todoDetails
+
+  const onDelete = () => {
+    deleteTodo(id)
+  }
+
+  return (
+    <li className="todo-list">
+      <p className="todo">{title}</p>
+      <button className="delete-btn" type="button" onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
